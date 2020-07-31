82082047ec9de0fabf521518dd0e2182922290db
jairovera17 <jairovera17@gmail.com>
2020-07-30 19:14:48 -0500

    Crear nuevo deck

--- deck.go
+++ deck.go
@@ -4,6 +4,20 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, values := range cardValues {
+			cards = append(cards, values+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
