426af7ba681b9198c681bec7798bf5df1db0375d
Jairo Vera <jairovera17@gmail.com>
2020-08-01 22:35:32 -0500

    Rutinas go

--- main.go
+++ main.go
@@ -3,14 +3,48 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
+	"time"
 )
 
 func main() {
-	resp, err := http.Get("http://google.com")
-	if err != nil {
-		fmt.Println("Error", err)
-		os.Exit(0)
+	links := []string{
+		"http://google.com",
+		"http://apple.com",
+		"http://facebook.com",
+		"http://amazon.com",
 	}
-	fmt.Println(resp)
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	// for i := 0; i < len(links); i++ {
+	// 	//<-c  is blocking
+	// 	fmt.Println(<-c)
+	// }
+
+	for l := range c {
+
+		//go checkLink(l, c)
+		go func(l string) {
+			time.Sleep(5 * time.Second)
+			checkLink(l, c)
+		}(l)
+	}
+
+}
+
+func checkLink(link string, c chan string) {
+
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(err, "might be down")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up")
+	c <- link
+
 }
