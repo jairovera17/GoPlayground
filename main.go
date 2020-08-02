c9deae694c98e62ef6d3f8eee4c3439134aed184
Jairo Vera <jairovera17@gmail.com>
2020-08-01 20:15:07 -0500

    Agregar tupla en mapa

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff000",
-		"green": "#4bf745",
-	}
+	colors := make(map[int]string)
+	colors[10] = "#ffffff"
+	delete(colors, 10)
+	// colors := map[string]string{
+	// 	"red":   "#ff000",
+	// 	"green": "#4bf745",
+	// }
 
 	fmt.Println(colors)
 }
